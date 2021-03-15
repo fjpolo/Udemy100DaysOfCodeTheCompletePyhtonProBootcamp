e6f842dcd22db7ff4a5ab6d083ce044718202d96
franco.polo <franco.polo@ii-vi.com>
2021-03-15 14:41:21 +0100

    Hangman done!

--- Day007/project/hangman/mainFinal.py
+++ Day007/project/hangman/mainFinal.py
@@ -2,21 +2,26 @@
 # Imports
 #
 import random
-import HangmanArt.py
-import HangmanWords.py
+
 
 #
 # main
 #
 if __name__ == "__main__":
+  #Step 5
   #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
   #Delete this line: word_list = ["ardvark", "baboon", "camel"]
+  from HangmanWords import word_list
+
   chosen_word = random.choice(word_list)
   word_length = len(chosen_word)
+
   end_of_game = False
   lives = 6
 
   #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+  from HangmanArt import logo
+  print(logo)
 
   #Testing code
   print(f'Pssst, the solution is {chosen_word}.')
@@ -30,17 +35,21 @@ if __name__ == "__main__":
     guess = input("Guess a letter: ").lower()
 
     #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
 
     #Check if user is wrong.
     if guess not in chosen_word:
         #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
         lives -= 1
         if lives == 0:
             end_of_game = True
@@ -55,4 +64,5 @@ if __name__ == "__main__":
         print("You win.")
 
     #TODO-2: - Import the stages from hangman_art.py and make this error go away.
-    print(stages[lives])    
+    from HangmanArt import stages
+    print(stages[lives])
