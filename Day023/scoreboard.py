c09a87dd8b25abee8b22f4c0827546ff1943ff4d
Franco Polo <fjpolo@gmail.com>
2021-04-25 18:19:42 +0200

    Done.-

---
+++ Day023/scoreboard.py
@@ -0,0 +1,31 @@
+#
+# Includes
+#
+from turtle import Turtle
+
+#
+# Constants
+#
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.hideturtle()
+        self.goto(-280, 260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=FONT)
